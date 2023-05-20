257aa7adc93a15cab50f004a9245fe4c95b0fc57
Ismail2hussein <ihusseinmohamed629@gmail.com>
2023-05-20 13:58:36 -0700

    Update challenge.js

--- challenge.js
+++ challenge.js
@@ -1,2 +1,14 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+const A = 10;
+const B = 20;
+const C = 30;
+
+// Use if statements to determine the largest number
+if (A > B && A> C) {
+  console.log(`${A} is the largest number`);
+} else if (B > A && B > C) {
+  console.log(`${B} is the largest number`);
+} else {
+  console.log(`${C} is the largest number`);
+}
